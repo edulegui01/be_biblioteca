ef4b618d048913380d5608940d9b641d171bd87f
Eduardo <edulegui01@gmail.com>
2025-05-15 22:47:57 -0300

    cambios en la tabla users y se agrego la logica de jsonwebtoken

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -1,28 +1,57 @@
 import { UserModel } from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-
+import jwt from 'jsonwebtoken'
 
 const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body
 
-    try{
-        const { username,password } = req.body;
+        if (!username || !email || !password) {
+            return res.status(400).json({ ok: false, msg: "Missing required fields: email, password, username" })
+        }
 
-        if (!username || !password) {
-            return res.status(400).json({ ok: false, msg: "Missing required fields: password, username" })
+        const user = await UserModel.findOneByEmail(email)
+        if (user) {
+            return res.status(409).json({ ok: false, msg: "Email already exists" })
         }
 
         const salt = await bcryptjs.genSalt(10)
         const hashedPassword = await bcryptjs.hash(password, salt)
 
-        const newUser = await UserModel.create({ username, password: hashedPassword })
+        const newUser = await UserModel.create({ email, password: hashedPassword, username })
 
-        return res.status(201).json({ ok: true, msg: "User created successfully", user: newUser })
+        const token = jwt.sign({ email: newUser.email, role_id: newUser.role_id },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h"
+            }
+        )
+
+        return res.status(201).json({
+            ok: true,
+            msg: {
+                token, role_id: newUser.role_id
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error server'
+        })
     }
-    catch(error){
+}
+
+const login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+    }catch(error){
         console.log(error)
         return res.status(500).json({ ok: false, msg: "Error creating user", error: error.message })
     }
 }
+
+
 
 
 
--- src/app/models/user.model.js
+++ src/app/models/user.model.js
@@ -1,58 +1,71 @@
 import db from '../config/db.js';
 
-const create = async ({ password, username }) => {
-    const query = {
-        text: `
-        INSERT INTO users (password, username)
-        VALUES ($1, $2)
-        RETURNING username, user_id, role_id
-        `,
-        values: [password, username]
-    }
+const create = async ({ email, password, username }) => {
+  const query = {
+      text: `
+      INSERT INTO users (email, password, username)
+      VALUES ($1, $2, $3)
+      RETURNING email, username, uid, role_id
+      `,
+      values: [email, password, username]
+  }
 
-    const { rows } = await db.query(query)
-    return rows[0]
+  const { rows } = await db.query(query)
+  return rows[0]
+}
+
+const findOneByEmail = async (email) => {
+  const query = {
+      text: `
+      SELECT * FROM users
+      WHERE EMAIL = $1
+      `,
+      values: [email]
+  }
+  const { rows } = await db.query(query)
+  return rows[0]
 }
 
 const findAll = async () => {
-    const query = {
-        text: `
-        SELECT * FROM users
-        `
-    }
-    const { rows } = await db.query(query)
-    return rows
+  const query = {
+      text: `
+      SELECT * FROM users
+      `
+  }
+  const { rows } = await db.query(query)
+  return rows
 }
 
 const findOneByUid = async (uid) => {
-    const query = {
-        text: `
-        SELECT * FROM users
-        WHERE user_id = $1
-        `,
-        values: [uid]
-    }
-    const { rows } = await db.query(query)
-    return rows[0]
+  const query = {
+      text: `
+      SELECT * FROM users
+      WHERE uid = $1
+      `,
+      values: [uid]
+  }
+  const { rows } = await db.query(query)
+  return rows[0]
 }
 
 const updateRoleVet = async (uid) => {
-    const query = {
-        text: `
-        UPDATE users
-        SET role_id = 2
-        WHERE user_id = $1
-        RETURNING *
-        `,
-        values: [uid]
-    }
-    const { rows } = await db.query(query)
-    return rows[0]
+  const query = {
+      text: `
+      UPDATE users
+      SET role_id = 2
+      WHERE uid = $1
+      RETURNING *
+      `,
+      values: [uid]
+  }
+  const { rows } = await db.query(query)
+  return rows[0]
 }
 
 export const UserModel = {
-    create,
-    findAll,
-    findOneByUid,
-    updateRoleVet
+  create,
+  findOneByEmail,
+  findAll,
+  findOneByUid,
+  updateRoleVet
 }
